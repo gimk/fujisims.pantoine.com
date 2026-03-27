5a70b5f3bcfa2c841c2d7184b3d6753d1c02ab35
gimk <a.pouligny@gmail.com>
2026-03-27 23:30:41 +0100

    refactor: Centralize slider parameter min/max/increment configuration within the `SliderParam` component.

--- src/content.config.ts
+++ src/content.config.ts
@@ -17,9 +17,6 @@ const recipesCollection = defineCollection({
         adjustments: z.array(z.object({
             label: z.string(),
             value: z.union([z.string(), z.number()]),
-            min: z.number().optional(),
-            max: z.number().optional(),
-            increment: z.number().optional(),
         })),
         images: z.array(image())
     })
